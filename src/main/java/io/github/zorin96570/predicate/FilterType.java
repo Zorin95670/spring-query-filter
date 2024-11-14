3feb698e25d273bfbd41e02becc0f9305b645743
Vincent Moittié <moittie.vincent@gmail.com>
2024-11-14 14:21:09 +0100

    Add annotation for entities

---
+++ src/main/java/io/github/zorin96570/predicate/FilterType.java
@@ -0,0 +1,45 @@
+package io.github.zorin96570.predicate;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Annotation to specify the filter type associated with a field.
+ * <p>
+ * This annotation allows the user to define a filter type for a field in a class. The filter type helps
+ * identify how a field should be handled when applying query filters in a filtering mechanism (e.g.,
+ * query parameters in an API request).
+ * </p>
+ * <p>
+ * The `type` attribute specifies the type of the filter, which can be used for processing the filter values
+ * dynamically.
+ * </p>
+ *
+ * Example usage:
+ * <pre>
+ * public class MyEntity {
+ *
+ *     @FilterType(type = String.class)
+ *     private String name;
+ *
+ *     @FilterType(type = Integer.class)
+ *     private Integer age;
+ * }
+ * </pre>
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.FIELD)
+public @interface FilterType {
+
+    /**
+     * Defines the filter type associated with the annotated field.
+     * <p>
+     * This value can be used to determine how the field should be processed during query filtering.
+     * </p>
+     *
+     * @return the filter type as a string
+     */
+    Class<?> type();
+}
