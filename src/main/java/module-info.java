a022ca045b7456f390086748736d7238ae431c49
Vincent Moittié <moittie.vincent@gmail.com>
2024-11-19 10:53:09 +0100

    Add missing module-info

---
+++ src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module io.github.zorin95670 {
+    exports io.github.zorin95670.exception;
+    exports io.github.zorin95670.predicate;
+    exports io.github.zorin95670.query;
+    exports io.github.zorin95670.specification;
+
+    requires transitive jakarta.persistence;
+    requires spring.data.commons;
+    requires spring.data.jpa;
+}
