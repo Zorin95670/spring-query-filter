a412ec15eff0fcf9b5942f87f4a42bbdce8e8fb6
Vincent Moittié <moittie.vincent@gmail.com>
2024-11-25 10:48:12 +0100

    Remove SpringQueryFilter class that manage pagination

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,6 @@
 module io.github.zorin95670 {
     exports io.github.zorin95670.exception;
     exports io.github.zorin95670.predicate;
-    exports io.github.zorin95670.query;
     exports io.github.zorin95670.specification;
 
     requires transitive jakarta.persistence;
